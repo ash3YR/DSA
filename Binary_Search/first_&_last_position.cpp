7a51e5b7b355612d11cfce17b983400da5158118
ash3YR <yash030m@gmail.com>
2025-05-24 16:53:04 +0530

    ss

---
+++ Binary_Search/first_&_last_position.cpp
@@ -0,0 +1,104 @@
+//solution 1: By lower bound and upper bound
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int lowerBound(vector<int> arr, int n, int x) {
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] >= x) {
+            ans = mid;
+            //look for smaller index on the left
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1; // look on the right
+        }
+    }
+    return ans;
+}
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int upperBound(vector<int> &arr, int n, int x) {
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] > x) {
+            ans = mid;
+            //look for smaller index on the left
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1; // look on the right
+        }
+    }
+    return ans;
+}
+
+
+pair<int, int> firstAndLastPosition(vector<int>& arr, int x) {
+    int n = arr.size();
+    int lb = lowerBound(arr, n, x);
+    int ub= upperBound(arr, n, x) - 1; // upperBound gives the index of the first element greater than x
+
+
+    if (lb < n && arr[lb] == x) {
+        return {lb, ub};
+    } else {
+        return {-1, -1}; // x not found
+    }
+}
+
+
+
+
+//Solution 2: By binary search   (Striver waali sheet me bss last occurence ka tha)
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int solve(int n, int key, vector < int > & v) {
+  int start = 0;
+  int end = n - 1;
+  int res = -1;
+
+  while (start <= end) {
+    int mid = start + (end - start) / 2;
+    if (v[mid] == key) {
+      res = mid;
+      start = mid + 1;
+
+    } else if (key < v[mid]) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return res;
+}
+
+int main() {
+  int n = 7;
+  int key = 13;
+  vector < int > v = {3,4,13,13,13,20,40};
+  
+  // returning the last occurrence index if the element is present otherwise -1
+  cout << solve(n, key, v) << "\n";
+
+  return 0;
+}
+
